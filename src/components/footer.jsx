f25d40582e89b3ac0f96c2c5c3fda32039cb17cb
MOHD RAVISH <mohdravish2003@gmail.com>
2023-06-18 17:36:04 +0530

    #1 commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+var date = new Date();
+
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright ⓒ {date.getFullYear()}</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <note>
+            <h1>This is the header</h1>
+            <p>This is the content</p>
+        </note>
+    )
+}
+
+export default Note;
